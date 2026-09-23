05eae88bc433c7776a60d32d29761f2ef4e51766
durga <durgaprasaddp184@gmail.com>
2026-09-23 14:53:41 +0530

    updated message enums

--- src/enums/helper.ts
+++ src/enums/helper.ts
@@ -12,6 +12,20 @@ export enum SETTINGS_TABS {
 }
 
 export enum MESSAGES {
+    SAVE_LOGIN_FORM_FIELDS_FAILED = 'Failed to save login form fields.',
+    FETCH_LOGIN_FORM_FIELDS_FAILED = 'Failed to fetch login form fields.',
+    SAVE_OR_FETCH_USER_FAILED = 'Failed to save or fetch user.',
+
+    SAVE_TAG_FORM_FIELDS_SUCCESS = 'Tag form fields saved successfully.',
+    SAVE_TAG_FORM_FIELDS_FAILED = 'Failed to save tag form fields.',
+    FETCH_TAG_FORM_FIELDS_FAILED = 'Failed to fetch tag form fields.',
+    FETCH_TAG_FORM_FIELDS_SUCCESS = 'Tag form fields fetched successfully.',
+    FETCH_USER_TAGS_FAILED = 'Failed to fetch user tags.',
+    FETCH_USER_TAGS_SUCCESS = 'User tags fetched successfully.',
+    DELETE_USER_TAG_FAILED = 'Failed to delete user tag.',
+    DELETE_USER_TAG_SUCCESS = 'User tag deleted successfully.',
+    SAVE_USER_TAG_FAILED = 'Failed to save user tag.',
+    SAVE_USER_TAG_SUCCESS = 'User tag saved successfully.',
     LOGIN_SUCCESS = 'Login successful.',
     LOGIN_FAILED = 'Login failed',
     LOGOUT_SUCCESS = 'Logout successful.',
